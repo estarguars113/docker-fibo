f743822c0710df2ba724f3c5a026937a3a6c4149
estarguars113 <estarguars113@gmail.com>
2022-11-12 07:25:40 +0200

    Updated to fix api issues

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -32,7 +32,7 @@ class Fib extends Component {
             index: this.state.index
         });
         this.setState({ index: '' });
-    };
+    }
 
     renderSeenIndexes() {
         return this.state.seenIndexes.map(({ number }) => number).join(', ');
